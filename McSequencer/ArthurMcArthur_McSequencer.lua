691a52e75aba0c2a71058dc3295b505327419f54
Arthur McArthur <arthurmcarthur@gmail.com>
2024-04-07 13:31:38 -0400

    1

--- McSequencer/ArthurMcArthur_McSequencer.lua
+++ McSequencer/ArthurMcArthur_McSequencer.lua
@@ -1,12 +1,9 @@
 -- @description Arthur McArthur McSequencer
 -- @author Arthur McArthur
 -- @license GPL v3
--- @version 1.1.20
+-- @version 1.1.21
 -- @changelog
---  New dropdown control and slider implementation
---  Added note pitch and start offset sliders
---  Alt-dragging on sliders now resets the value to default
---  Bug fixes
+--  Better dependency checks (thanks Daniel Lumertz and BirdBird)
 -- @provides
 --   Modules/*.lua
 --   Images/*.png
@@ -15,40 +12,160 @@
 --   Fonts/*.ttc
 --   [effect] JSFX/*.jsfx
 
-local versionNumber = '1.1.20'
+local versionNumber = '1.1.21'
 local reaper = reaper
 local os = reaper.GetOS()
- 
-local function checkDependencies()
-    local missingDeps = {}
 
-    -- Check for ReaImGui
-    if not reaper.ImGui_GetVersion or reaper.ImGui_GetVersion() < "0.8.7.5" then
-        missingDeps[#missingDeps + 1] = '"ReaImGui (found in ReaPack)"'
-    end
 
-    -- Check for SWS extension
-    if not reaper.CF_GetSWSVersion or reaper.CF_GetSWSVersion() < "2.12.1" then
-        missingDeps[#missingDeps + 1] = 'SWS extension, found at https://www.sws-extension.org/'
-    end
 
-    if not reaper.JS_ReaScriptAPI_Version() then
-        missingDeps[#missingDeps + 1] = 'JS_ReaScriptAPI extension (found in ReaPack)'
-    end
-
-    if #missingDeps > 0 then
-        local missingDepsStr = table.concat(missingDeps, " and ")
-        reaper.ShowMessageBox(
-        "This script requires " .. missingDepsStr .. ".\nPlease install them.",
-            "Missing Dependencies", 0)
-        -- reaper.ReaPack_BrowsePackages(missingDepsStr)
-        return true
-    end
-
-    return false
+function GetFullProjectPath(proj) -- with projct Name. with .rpp at the end
+    return reaper.GetProjectPathEx( proj ):gsub("(.*)\\.*$","%1")  .. reaper.GetProjectName(proj)
 end
 
-if checkDependencies() then return end
+
+---------------------
+----------------- Extension Checker 
+---------------------
+
+--- Check if user REAPER version. min_version and max_version are optional
+function CheckREAPERVersion(min_version, max_version)
+    local reaper_version = reaper.GetAppVersion():match('%d+%.%d+')
+    local bol, why = CompareVersion(reaper_version, min_version, max_version)
+    if not bol then
+        local text
+        if why == 'min' then
+            text = 'This script requires that REAPER minimum version be : '..min_version..'.\nYour version is '..reaper_version
+        elseif why == 'max' then
+            text = 'This script requires that REAPER maximum version be : '..max_version..'.\nYour version is '..reaper_version
+        end
+        reaper.ShowMessageBox(text, 'Error - REAPER at Incompatible Version', 0)
+        return false
+    end
+    return true
+end
+
+--- Check if user have Extension. min_version and max_version are optional
+function CheckSWS(min_version, max_version)
+    if not reaper.APIExists('CF_GetSWSVersion') then
+        local text = 'This script requires the SWS Extension to run. \nWould you like to be redirected to the SWS Extension website to install it?'
+        local ret = reaper.ShowMessageBox(text, 'Error - Missing Dependency', 4)
+        if ret == 6 then
+            open_url('https://www.sws-extension.org/')
+        end
+        return false
+    else
+        local sws_version = reaper.CF_GetSWSVersion()
+        local bol, why = CompareVersion(sws_version, min_version, max_version)
+        if not bol then
+            local text, url
+            if why == 'min' then
+                text = 'This script requires that SWS minimum version be : '..min_version..'.\nYour version is '..sws_version..'.\nWould you like to be redirected to the SWS Extension website to update it?'
+                url = 'https://www.sws-extension.org/'
+            elseif why == 'max' then
+                text = 'This script requires that SWS maximum version be : '..max_version..'.\nYour version is '..sws_version..'.\nWould you like to be redirected to the website of old version SWS Extension to downgrade it?'
+                url = 'https://www.sws-extension.org/download/old/'
+            end
+            local ret = reaper.ShowMessageBox(text, 'Error - Missing Dependency', 4)
+            if ret == 6 then
+                open_url(url)
+            end
+            return false
+        end
+    end
+    return true
+end
+
+--- Check if user have Extension. min_version and max_version are optional
+function CheckReaImGUI(min_version, max_version)
+    if not reaper.APIExists('ImGui_GetVersion') then
+        local text = 'This script requires the ReaImGui extension to run. You can install it through ReaPack.'
+        local ret = reaper.ShowMessageBox(text, 'Error - Missing Dependency', 0)
+        return false
+    else
+        local imgui_version, imgui_version_num, reaimgui_version = reaper.ImGui_GetVersion()
+        local bol, why = CompareVersion(reaimgui_version, min_version, max_version)
+        if not bol then
+            local text
+            if why == 'min' then
+                text = 'This script requires that ReaImgui minimum version be : '..min_version..'.\nYour version is '..reaimgui_version..'\nYou can update it through ReaPack.'
+            elseif why == 'max' then
+                text = 'This script requires that ReaImgui maximum version be : '..max_version..'.\nYour version is '..reaimgui_version..'\nYou can update it through ReaPack.'
+            end
+            reaper.ShowMessageBox(text, 'Error - Dependency at Incompatible Version', 0)
+            return false
+        end
+    end    
+    return true
+end
+
+--- Check if user have Extension. min_version and max_version are optional
+function CheckJS(min_version, max_version)
+    if not reaper.APIExists('JS_ReaScriptAPI_Version') then
+        local text = 'This script requires the js_ReaScriptAPI extension to run. You can install it through ReaPack.'
+        local ret = reaper.ShowMessageBox(text, 'Error - Missing Dependency', 0)
+        return false
+    else
+        local js_version = tostring(reaper.JS_ReaScriptAPI_Version())
+        local bol, why = CompareVersion(js_version, min_version, max_version)
+        if not bol then
+            local text
+            if why == 'min' then
+                text = 'This script requires that js_ReaScriptAPI minimum version be : '..min_version..'.\nYour version is '..js_version..'\nYou can update it through ReaPack.'
+            elseif why == 'max' then
+                text = 'This script requires that js_ReaScriptAPI maximum version be : '..max_version..'.\nYour version is '..js_version..'\nYou can update it through ReaPack.'
+            end
+            reaper.ShowMessageBox(text, 'Error - Dependency at Incompatible Version', 0)
+            return false
+        end
+    end
+    return true
+end
+
+function CompareVersion(check_version, min_version, max_version, separator)
+    separator = separator or '.'
+    local check_table = {}
+    for version in check_version:gmatch('(%d+)'..separator..'?') do
+        check_table[#check_table+1] = tonumber(version)
+    end
+
+    local min_table
+    if min_version then
+        min_table = {}
+        for version in min_version:gmatch('(%d+)'..separator..'?') do
+            min_table[#min_table+1] = tonumber(version)
+        end
+    end
+
+    local max_table
+    if max_version then
+        max_table = {}
+        for version in max_version:gmatch('(%d+)'..separator..'?') do
+            max_table[#max_table+1] = tonumber(version)
+        end
+    end
+
+    for index, check_v in ipairs(check_table) do
+        -- check if is less than the min_version
+        if min_table and check_v < (min_table[index] or 0) then
+            return false, 'min'
+        elseif min_table and check_v > (min_table[index] or 0) then -- bigger than the min version stop checking min_version
+            min_table = nil
+        end
+        
+        -- check if is more than the max_version
+        if max_table and check_v > (max_table[index] or 0) then
+            return false, 'max'
+        elseif max_table and check_v < (max_table[index] or 0) then -- less than the max version stop checking max_version
+            max_table = nil
+        end    
+    end
+
+    return true, nil
+end
+ 
+CheckJS()
+CheckSWS()
+CheckReaImGUI()
 
 local function print(v) reaper.ShowConsoleMsg("\n" .. tostring(v)) end
 
@@ -692,7 +809,9 @@ local function obj_Knob2(ctx, imageParams, id, value, params, mouse, keys, yOffs
             mouse_x, mouse_y = reaper.GetMousePosition()
             trackDeltaX = mouse_x - dragStartPos[id].x
             trackDeltaY = mouse_y - dragStartPos[id].y    
-            reaper.JS_Mouse_SetPosition(dragStartPos[id].x, dragStartPos[id].y)
+            if  reaper.JS_ReaScriptAPI_Version then
+                reaper.JS_Mouse_SetPosition(dragStartPos[id].x, dragStartPos[id].y)
+            end
         else
             trackDeltaX = mouse.delta_x
             trackDeltaY = mouse.delta_y
